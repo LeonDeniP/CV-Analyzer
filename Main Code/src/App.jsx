0a7fdd50f279d8fc4711ba9576f58265949244e6
Leon <leonardusprabowo14@gmail.com>
2026-05-27 12:57:33 +0700

    edit app.js

--- Main Code/src/App.jsx
+++ Main Code/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
-import Header from "./components/layout/Header";
-import Footer from "./components/layout/Footer";
+import Header from "./components/layout/header.jsx";
+import Footer from "./components/layout/footer.jsx";
 
 import constants, {
   buildPresenceChecklist,
